be6036feb37dcaaa6373fef2860455c5d314f9ae
Damien Maillard <damien.maillard@dailymotion.com>
2019-06-14 18:14:24 +0200

    prepare uneval even more

--- src/decompose.js
+++ src/decompose.js
@@ -4,116 +4,145 @@
  */
 
 import { isComposite } from "./is-composite.js"
+import { getCompositeGlobalPath, getPrimitiveGlobalPath } from "./global-value-path.js"
 
-export const decompose = (rootValue) => {
-  const composites = []
-  const primitives = []
-  const links = []
+export const decompose = (mainValue) => {
+  const values = []
+  const recipes = []
 
-  const visit = (value) => {
-    if (isComposite(value)) {
-      const existingCompositeIndex = composites.indexOf(value)
+  const valueToRecipeIdentifier = (value) => {
+    if (!isComposite(value)) {
+      const existingPrimitiveIndex = values.findIndex((existingValue) => {
+        if (Object.is(value, existingValue)) return true
+        return value === existingValue
+      })
+      if (existingPrimitiveIndex > -1) return existingPrimitiveIndex
+      const recipeIdentifier = values.length
+      values.push(value)
 
-      if (existingCompositeIndex === -1) {
-        const identifier = composites.length
-        composites.push(value)
+      if (typeof value === "symbol") {
+        const symbolGlobalPath = getPrimitiveGlobalPath(value)
+        if (!symbolGlobalPath)
+          throw new Error(createUnexpectedSymbolErrorMessage({ symbol: value }))
 
-        // for date and regexp we need valueOf
-        // by default we'll consider if valueOf returns a primitive
-        // it must be passed to the constructor
-        if ("valueOf" in value && typeof value.valueOf === "function") {
-          // usefull because new Date(10).valueOf() === 10
-          // or new Boolean(true).valueOf() === true
-          const valueOfReturnValue = value.valueOf()
-          if (isComposite(valueOfReturnValue)) {
-            if (valueOfReturnValue !== value) {
-              throw new Error(
-                `cannot decompose an object having a valueOf method returning a different composite`,
-              )
-            }
-          } else {
-            const valueOfReturnValueVisitResult = visit(valueOfReturnValue)
-            const valueOfLink = {
-              fromCompositeIdentifier: identifier,
-              toDescription: valueOfReturnValueVisitResult,
-              data: {
-                type: "valueOf",
-              },
-            }
-            links.push(valueOfLink)
-          }
+        recipes[recipeIdentifier] = {
+          type: "global-reference",
+          globalPath: symbolGlobalPath,
         }
-
-        const prototype = Object.getPrototypeOf(value)
-        if (prototype) {
-          const prototypeVisitResult = visit(prototype)
-          const prototypeLink = {
-            fromCompositeIdentifier: identifier,
-            toDescription: prototypeVisitResult,
-            data: {
-              type: "prototype",
-            },
-          }
-          links.push(prototypeLink)
-        }
-
-        const propertyNamesLinks = []
-        Object.getOwnPropertyNames(value).forEach((propertyName) => {
-          const propertyDescriptor = Object.getOwnPropertyDescriptor(value, propertyName)
-          Object.keys(propertyDescriptor).forEach((descriptorName) => {
-            const descriptorValue = propertyDescriptor[descriptorName]
-            const descriptorValueVisitResult = visit(descriptorValue)
-            propertyNamesLinks.push({
-              fromCompositeIdentifier: identifier,
-              toDescription: descriptorValueVisitResult,
-              data: {
-                type: "property",
-                propertyName,
-                descriptorName,
-              },
-            })
-          })
-        })
-        links.push(...propertyNamesLinks)
-
-        const symbolsLinks = []
-        Object.getOwnPropertySymbols(value).forEach((symbol) => {
-          const propertyDescriptor = Object.getOwnPropertyDescriptor(value, symbol)
-          Object.keys(propertyDescriptor).forEach((descriptorName) => {
-            const descriptorValue = propertyDescriptor[descriptorName]
-            const descriptorValueVisitResult = visit(descriptorValue)
-            symbolsLinks.push({
-              fromCompositeIdentifier: identifier,
-              toDescription: descriptorValueVisitResult,
-              data: {
-                type: "symbol",
-                symbol,
-                descriptorName,
-              },
-            })
-          })
-        })
-        links.push(...symbolsLinks)
-
-        return { type: "composite", identifier }
+        return recipeIdentifier
       }
-
-      return { type: "composite", identifier: existingCompositeIndex }
+      recipes[recipeIdentifier] = {
+        type: "primitive",
+        value,
+      }
+      return recipeIdentifier
     }
 
-    const existingPrimitiveIndex = primitives.findIndex((existingValue) => {
-      if (Object.is(value, existingValue)) return true
-      return value === existingValue
+    const existingCompositeIndex = values.indexOf(value)
+    if (existingCompositeIndex > -1) return existingCompositeIndex
+    const recipeIdentifier = values.length
+    values.push(value)
+
+    const compositeGlobalPath = getCompositeGlobalPath(value)
+    if (compositeGlobalPath) {
+      recipes[recipeIdentifier] = {
+        type: "global-reference",
+        globalPath: compositeGlobalPath,
+      }
+      return recipeIdentifier
+    }
+
+    const recipeParts = {}
+
+    if ("valueOf" in value && typeof value.valueOf === "function") {
+      // usefull because new Date(10).valueOf() === 10
+      // or new Boolean(true).valueOf() === true
+      const valueOfReturnValue = value.valueOf()
+      if (isComposite(valueOfReturnValue)) {
+        if (valueOfReturnValue !== value) {
+          throw new Error(
+            `cannot decompose an object having a valueOf method returning a different composite`,
+          )
+        }
+      } else {
+        const valueOfRecipeIdentifier = valueToRecipeIdentifier(valueOfReturnValue)
+        recipeParts.valueOfRecipeIdentifier = valueOfRecipeIdentifier
+      }
+    }
+
+    const propertyRecipeArray = []
+    Object.getOwnPropertyNames(value).forEach((propertyName) => {
+      const propertyNameRecipeIdentifier = valueToRecipeIdentifier(propertyName)
+      const propertyDescriptor = Object.getOwnPropertyDescriptor(value, propertyName)
+      Object.keys(propertyDescriptor).forEach((descriptorName) => {
+        const descriptorNameRecipeIdentifier = valueToRecipeIdentifier(descriptorName)
+        const descriptorValueRecipeIdentifier = valueToRecipeIdentifier(
+          propertyDescriptor[descriptorName],
+        )
+        propertyRecipeArray.push({
+          propertyNameRecipeIdentifier,
+          descriptorNameRecipeIdentifier,
+          descriptorValueRecipeIdentifier,
+        })
+      })
     })
-    if (existingPrimitiveIndex === -1) {
-      const identifier = primitives.length
-      primitives.push(value)
-      return { type: "primitive", identifier }
+    recipeParts.propertyRecipeArray = propertyRecipeArray
+
+    const symbolRecipeArray = []
+    Object.getOwnPropertySymbols(value).forEach((symbol) => {
+      const symbolRecipeIdentifier = valueToRecipeIdentifier(symbol)
+      const propertyDescriptor = Object.getOwnPropertyDescriptor(value, symbol)
+      Object.keys(propertyDescriptor).forEach((descriptorName) => {
+        const descriptorNameRecipeIdentifier = valueToRecipeIdentifier(descriptorName)
+        const descriptorValueRecipeIdentifier = valueToRecipeIdentifier(
+          propertyDescriptor[descriptorName],
+        )
+        symbolRecipeArray.push({
+          symbolRecipeIdentifier,
+          descriptorNameRecipeIdentifier,
+          descriptorValueRecipeIdentifier,
+        })
+      })
+    })
+    recipeParts.symbolRecipeArray = symbolRecipeArray
+
+    const prototypeValue = Object.getPrototypeOf(value)
+    if (prototypeValue === null) {
+      recipeParts.prototypeRecipeIdentifier = valueToRecipeIdentifier(prototypeValue)
+    } else {
+      const prototypeIndex = values.indexOf(prototypeValue)
+      if (prototypeIndex > -1) {
+        recipeParts.prototypeRecipeIdentifier = prototypeIndex
+      } else {
+        const prototypeRecipeIdentifier = values.length
+        values.push(prototypeValue)
+        const prototypeGlobalPath = getCompositeGlobalPath(prototypeValue)
+        if (prototypeGlobalPath) {
+          recipes[prototypeRecipeIdentifier] = {
+            type: "global-reference",
+            globalPath: prototypeGlobalPath,
+          }
+          recipeParts.prototypeRecipeIdentifier = prototypeRecipeIdentifier
+        } else {
+          throw new Error(createUnexpectedPrototypeErrorMessage({ prototypeValue }))
+        }
+      }
     }
 
-    return { type: "primitive", identifier: existingPrimitiveIndex }
+    recipes[recipeIdentifier] = {
+      type: "composite",
+      ...recipeParts,
+    }
+    return recipeIdentifier
   }
-  visit(rootValue)
+  const mainRecipeIdentifier = valueToRecipeIdentifier(mainValue)
 
-  return { primitives, composites, links }
+  return { recipes, mainRecipeIdentifier }
 }
+
+const createUnexpectedSymbolErrorMessage = ({ symbol }) => `symbol must be a well known symbol.
+symbol: ${symbol}`
+
+const createUnexpectedPrototypeErrorMessage = ({ prototypeValue }) =>
+  `prototype must be a well known prototype or somewhere in the value.
+prototypeValue: ${prototypeValue}`
--- src/uneval.js
+++ src/uneval.js
@@ -1,1 +1,73 @@
-export const uneval = () => {}
+import { decompose } from "./decompose.js"
+
+export const uneval = (value) => {
+  const composition = decompose(value)
+  debugger
+  return produce(composition)
+}
+
+const produce = ({ recipes, mainRecipeIdentifier }) => {
+  const values = recipes.map((recipe) => followRecipe(recipe, recipes))
+  return values[mainRecipeIdentifier]
+}
+
+const followRecipe = (recipe, recipes) => {
+  if (recipe.type === "primitive") return followPrimitiveRecipe(recipe)
+  if (recipe.type === "global-reference") return followGlobalReferenceRecipe(recipe)
+  return followCompositeRecipe(recipe, recipes)
+}
+
+const followPrimitiveRecipe = ({ value }) => value
+
+const followGlobalReferenceRecipe = ({ globalPath }) => {
+  let currentValue = typeof window === "object" ? window : global
+  let i = 0
+  while (i < globalPath.length) {
+    const part = globalPath[i]
+    i++
+    if (part in currentValue === false) return undefined
+    currentValue = currentValue[part]
+  }
+  return currentValue
+}
+
+const followCompositeRecipe = (
+  { valueOfRecipeIdentifier, prototypeRecipeIdentifier, propertyRecipeArray, symbolRecipeArray },
+  recipes,
+) => {
+  if (valueOfRecipeIdentifier) {
+  }
+
+  // how do we ensure recipe are followed in the right order ?
+  // because there is a dependency order, prototype must be followed first
+  // and property name, descriptor name before being able to set the descriptor value
+
+  // how do we ensure new Date(10) instead of Object.create(Date.prototype) ?
+  // I guess we should store that it has a global constructor and use it instead
+  // of Object.create. We'll use Object.create only for composite having
+  // their prototype being Object.prototype at some point
+
+  // how do we follow property or symbol recipe because we must construct
+  // the whole descriptor first
+
+  // respect Object.preventExtension too
+
+  const prototypeValue = followRecipe(recipes[prototypeRecipeIdentifier], recipes)
+  const composite = Object.create(prototypeValue)
+
+  propertyRecipeArray.forEach((propertyRecipe) => {
+    const descriptor = {}
+    const propertyName = followRecipe(recipes[propertyRecipe.propertyNameRecipeIdentifier], recipes)
+    Object.defineProperty(composite, propertyName, descriptor)
+  })
+
+  symbolRecipeArray.forEach((symbolRecipe) => {
+    const descriptor = {}
+    const symbol = followRecipe(recipes[symbolRecipe.symbolRecipeIdentifier], recipes)
+    Object.defineProperty(composite, symbol, descriptor)
+  })
+
+  return composite
+}
+
+uneval({ foo: true })
